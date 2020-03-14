4d07a746bdbdc431f0030115b9d95b4c3af88db9
Alécio <alecio18@hotmail.com>
2020-03-14 03:45:17 -0300

    adicionado (próxima semana)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
 	<div id="app">
-		<h1>Tarefas</h1>
+		<h1>Semana Atual</h1>
 		<TaskProgress :progress="progress" />
 		<NewTask @taskAdded="addTask" />
 		<TaskGrid :tasks="tasks" 
 		@taskDeleted="deleteTask"
 		@taskStateChanged="toggleTaskState"
+		/>
+
+		<hr>
+
+		<h1>Próxima Semana</h1>
+		<TaskProgress :progress="progress2" />
+		<NewTask @taskAdded="addTask2" />
+		<TaskGrid :tasks="tasks2" 
+		@taskDeleted="deleteTask2"
+		@taskStateChanged="toggleTaskState2"
+
+
 		/>
 	</div>
 </template>
@@ -18,7 +30,8 @@ export default {
 	components: { TaskProgress, TaskGrid, NewTask },
 	data(){
 		return {
-			tasks: []				
+			tasks: [],
+			tasks2: []				
 		}
 	},
 	computed : {
@@ -26,7 +39,13 @@ export default {
 			const total = this.tasks.length
 			const done = this.tasks.filter(t => !t.pending).length
 			return Math.round(done / total * 100) || 0
-		}
+		},
+		progress2() {
+			const total = this.tasks2.length
+			const done = this.tasks2.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		},
+
 	},
 	watch: {
 		tasks: {
@@ -34,6 +53,13 @@ export default {
 			handler() {
 				localStorage.setItem('tasks', JSON.stringify(this.tasks))
 			}
+		},
+
+		tasks2 : {
+			deep: true,
+			handler() {
+				localStorage.setItem('tasks2', JSON.stringify(this.tasks2))
+		}
 			
 		}
 
@@ -54,13 +80,35 @@ export default {
 		},
 		toggleTaskState(i){
 			this.tasks[i].pending = !this.tasks[i].pending
+		},
+
+
+		addTask2(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks2.filter(sameName).length == 0
+			if(reallyNew) {
+				this.tasks2.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
+		},
+		deleteTask2(id) {
+			this.tasks2.splice(id, 1)
+		},
+		toggleTaskState2(i){
+			this.tasks2[i].pending = !this.tasks2[i].pending
 		}
 
 	},
 	created() {
-		const jsonDados = localStorage.getItem('tasks')
+		const jsonDados = localStorage.getItem('tasks')		
 		const array = JSON.parse(jsonDados)
 		this.tasks = Array.isArray(array) ? array : []
+
+		const jsonDados2 = localStorage.getItem('tasks2')		
+		const array2 = JSON.parse(jsonDados2)
+		this.tasks2 = Array.isArray(array2) ? array2 : []
 	}
 }
 </script>
